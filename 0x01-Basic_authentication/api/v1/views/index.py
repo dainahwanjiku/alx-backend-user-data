361a8e2abe6f247fcfbd9165be9e092f506933d2
dainahwanjiku <deeshish2017@gmail.com>
2023-02-08 00:19:04 +0000

    files

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+
+"""testing this new error handler, adding a new endpoint"""
+
+
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
