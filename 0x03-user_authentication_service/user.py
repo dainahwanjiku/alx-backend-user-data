82533c5a57a214ee3c6470d0092828299f1df6d6
dainahwanjiku <deeshish2017@gmail.com>
2023-02-16 10:02:51 +0000

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,10 +12,10 @@ class User(Base):
     """this is what is contained in the user class.
     """
     id = Column(Integer, primary_key=True)
-    email = Column(String, nulliable=False)
-    hashed_password = Column(String, nulliable=False)
-    session_id = Column(String, nulliable=True)
-    reset_token = Column(String, nulliable=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
 
     def __repr__(self):
         return "<User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
