23550c137a512b2c3561fa867c61ccb6ec2d8eab
dainahwanjiku <deeshish2017@gmail.com>
2023-02-16 12:36:02 +0000

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,11 +2,11 @@
 """
 create a SQLAlchemy model named User for a database table named users
 """
-
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from db import db
+
+
+Base = declarative_base()
 
 
 class User(Base):
@@ -15,11 +15,7 @@ class User(Base):
     """this is what is contained in the user class.
     """
     id = Base.Column(Integer, primary_key=True)
-    email = Base.Column(String, nullable=False)
-    hashed_password = Base.Column(String, nullable=False)
-    session_id = Base.Column(String, nullable=True)
-    reset_token = Base.Column(String, nullable=True)
-
-    def __repr__(self):
-        return "<User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
-                self.email, self.hashed_password, self.session_id, self.reset_token)
+    email = Base.Column(String(250), nullable=False)
+    hashed_password = Base.Column(String(250), nullable=False)
+    session_id = Base.Column(String(250), nullable=True)
+    reset_token = Base.Column(String(250), nullable=True)
